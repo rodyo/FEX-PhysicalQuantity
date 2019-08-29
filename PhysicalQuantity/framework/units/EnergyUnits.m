f2a1616a3450b4ba898cffa08618c832f1795aad
Rody Oldenhuis <rody@dawnaerospace.com>
2019-08-29 18:26:02 +1200

    * Added some mass units

--- PhysicalQuantity/framework/units/EnergyUnits.m
+++ PhysicalQuantity/framework/units/EnergyUnits.m
@@ -9,7 +9,7 @@ classdef EnergyUnits < UnitOfMeasurementType
                                           'long_name',  'Joule'); 
         
         other_units = get_energy_units()
-    end
+    end    
 end
 
 % Utility funcion provides an easy means to write all the available force units
@@ -19,15 +19,15 @@ function U = get_energy_units()
     % From: 
     % https://en.wikipedia.org/wiki/Units_of_energy
         
-    %    system                   short    long               long plural        conversion factor (to J)
-    S = {SystemOfUnits.subatomic  'eV'     'electronvolt'     'electronvolts'    1.602176634e-19
-         SystemOfUnits.cgs        'erg'    'erg'              'ergs'             1e-7
-         SystemOfUnits.imperial   'Btu'    'BTU'              'BTUs'             1055.06 % International standard ISO 31-4 on Quantities and units—Part 4: Heat
-         SystemOfUnits.imperial   'ft-lbf' 'foot pound-force' 'ft-lb'            1.3558179483314004 % (abusing the non-existent plural to get the variant of the short form in) 
-         SystemOfUnits.imperial   'hph'    'horsepower-hour'  'horsepower-hours' 2.6845e6
-         SystemOfUnits.metric     'cal'    'calorie'          'calories'         4.184  % small calorie
-         SystemOfUnits.metric     'Cal'    'Calorie'          'Calories'         4184   % large calorie == 1 kcal
-         SystemOfUnits.metric     'kWh'    'kilowatt-hour'    'kilowatt-hours'   3.6e6};
+    %    system                   short    long               long plural  conversion factor (to J)
+    S = {SystemOfUnits.subatomic  'eV'     'electronvolt'     ''           1.602176634e-19
+         SystemOfUnits.cgs        'erg'    'erg'              ''           1e-7
+         SystemOfUnits.imperial   'Btu'    'BTU'              ''           1055.06 % International standard ISO 31-4 on Quantities and units—Part 4: Heat
+         SystemOfUnits.imperial   'ft-lbf' 'foot pound-force' 'ft-lb'      1.3558179483314004 % (abusing the non-existent plural to get the variant of the short form in) 
+         SystemOfUnits.imperial   'hph'    'horsepower-hour'  ''           2.6845e6
+         SystemOfUnits.metric     'cal'    'calorie'          ''           4.184  % small calorie
+         SystemOfUnits.metric     'Cal'    'Calorie'          ''           4184   % large calorie == 1 kcal
+         SystemOfUnits.metric     'kWh'    'kilowatt-hour'    ''           3.6e6};
      
     % Now define the actual unit object
     U = DerivedUnitOfMeasurement('dimensions',            repmat({EnergyUnits.dimensions},size(S,1),1),...
--- PhysicalQuantity/framework/units/MassUnits.m
+++ PhysicalQuantity/framework/units/MassUnits.m
@@ -13,20 +13,25 @@ function U = get_mass_units()
     % Some abbreviations
     sane     = SystemOfUnits.metric;
     crazy    = SystemOfUnits.imperial;
-    retarded = SystemOfUnits.us_customary;
+    retarded = SystemOfUnits.us_customary;    
+    astro    = SystemOfUnits.astronomical;
+    atomic   = SystemOfUnits.subatomic;
 
-    %   system    symbol     short  long         conversion factor (to GRAM (not kg))
+    %   system    symbol           short  long               long plural     conversion factor (to GRAM (not kg))
     S = {  
-        sane      ''         't'    'tonne'      1e6
-        sane      ''         't'    'ton'        1e6 % (alias)
-        retarded  ''         ''     'short ton'  907.18474e3
-        crazy     ''         ''     'long ton'   1016.047e3
-        crazy     ''         'lb'   'pound'      0.45359237e3
-        retarded  ''         ''     'slug'       14593.90
-        crazy     ''         'st'   'stone'      6.35029318e3
-        crazy     char(8485) 'oz'   'ounce'      28.349523125
-        crazy     ''         'gr'   'grain'      64.79891e-3
-        crazy     ''         'ct'   'carat'      200e-3        
+        sane      ''               't'    'tonne'            ''              1e6
+        sane      ''               't'    'ton'              ''              1e6 % (alias)
+        retarded  ''               ''     'short ton'        ''              907.18474e3
+        crazy     ''               ''     'long ton'         ''              1016.047e3
+        crazy     ''               'lb'   'pound'            ''              0.45359237e3
+        retarded  ''               ''     'slug'             ''              14593.90
+        crazy     ''               'st'   'stone'            ''              6.35029318e3
+        crazy     char(8485)       'oz'   'ounce'            ''              28.349523125
+        crazy     ''               'gr'   'grain'            ''              64.79891e-3
+        crazy     ''               'ct'   'carat'            ''              200e-3     
+        atomic    ['m' char(8346)] 'mp'   'Planck mass'      'Planck masses' 2.176435e-5
+        atomic    'Da'             'u'    'Atomic mass unit' ''              1.66053906660e-24
+        astro     ['M' char(9737)] ''     'Solar mass'       'Solar masses'  1.98847e27
         };
      
     % Now define the actual unit object
@@ -34,7 +39,8 @@ function U = get_mass_units()
                                  'system',                  S(:,1)',...
                                  'symbol',                  S(:,2)',...
                                  'short_name',              S(:,3)',...
-                                 'long_name',               S(:,4)',...                                 
-                                 'conversion_to_base_unit', S(:,5)'); 
+                                 'long_name',               S(:,4)',...   
+                                 'long_name_plural_form',   S(:,5)',...                                 
+                                 'conversion_to_base_unit', S(:,6)'); 
     
 end
